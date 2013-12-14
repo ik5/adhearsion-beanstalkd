5a4affe9b1ffc8cd3c3c8555fd1f7897ff1d5638
ik5 <idok@linesip.com>
2013-12-14 02:04:57 +0200

    Working on initiating backburner

--- lib/adhearsion/beanstalkd/service.rb
+++ lib/adhearsion/beanstalkd/service.rb
@@ -5,7 +5,15 @@ module Adhearsion
     class Service
       class << self
         def start
+          Backburner.configure do |conf|
 
+          end
+
+          Backburner.work
+        end
+        
+        def config
+          @config ||= Adhearsion.config[:adhearsion_beanstalkd]
         end
 
       end
